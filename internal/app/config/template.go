34175588b68bb9f57cba1f3b7f5782226db3d10d
Pankaj Pawar] <pankaj91as@gmail.com>
2024-12-04 15:32:45 +0530

    template copy into container

--- internal/app/config/template.go
+++ internal/app/config/template.go
@@ -6,8 +6,8 @@ import (
 )
 
 func GetNotificationTemplate(QueueName, NotificationType string) (string, error) {
-	fmt.Printf("Reading template from %s", "./template/"+QueueName+"/"+NotificationType+".tmpl\n")
-	contents, err := os.ReadFile("./template/" + QueueName + "/" + NotificationType + ".tmpl")
+	fmt.Printf("Reading template from %s", "template/"+QueueName+"/"+NotificationType+".tmpl\n")
+	contents, err := os.ReadFile("template/" + QueueName + "/" + NotificationType + ".tmpl")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return "", err
